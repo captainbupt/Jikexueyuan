32911b0bfdf13f87b688729c2fa29332f03f7917
weizhou1 <weizhou1@bj-m-202614a.local>
2016-03-10 17:58:47 +0800

    Can take homework automatically

--- background.js
+++ background.js
@@ -1,36 +1,44 @@
 running = 0
 
 function qiang(id){
-	var id = $(this).data('id');
-	$.ajax({
-		url: 'http://fuwu.jikexueyuan.com/homework/take-homework',
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			id: id
-		},
-		success: function(data){
-			if(data.error === 0){
-				alert('«¿µ•≥…π¶');
-				window.location.href = window.APP_URL+'homework/'+id+'/correct';
-			} else {
-				alert(data.message);
+	chrome.cookies.get({"url": "http://fuwu.jikexueyuan.com", "name": "XSRF-TOKEN"}, function(cookie){
+		console.log(cookie.value);
+		$.ajax({
+			url: 'http://fuwu.jikexueyuan.com/homework/take-homework',
+			type: 'POST',
+			dataType: 'json',
+			headers: {
+				// modified this line every time
+				"X-CSRF-TOKEN": "S0Gw67kPEPJBUqRZoDgvueKgRjcVf7X8QmW4YUmE",
+				"X-Requested-With": "XMLHttpRequest"
+			},
+			data: {
+				id: id
+			},
+			success: function(data){
+				if(data.error === 0){
+					alert('Êä¢ÂçïÊàêÂäü');
+					//window.location.href = window.APP_URL+'homework/'+id+'/correct';
+				} else {
+					alert(data.message);
+				}
 			}
-		}
+		});
 	});
 }
 
 function notCorrected(){
 	var xhr = new XMLHttpRequest();
-	//xhr.open("GET", "http://fuwu.jikexueyuan.com/homework/not-corrected", false);
-	xhr.open("GET", "http://fuwu.jikexueyuan.com/homework/my", false);
+	xhr.open("GET", "http://fuwu.jikexueyuan.com/homework/not-corrected", false);
+	//xhr.open("GET", "http://fuwu.jikexueyuan.com/homework/my", false);
 	xhr.send();
 	var response = xhr.responseText.replace(/\n| |\r|\t/g, "");
 	
 	re = /<td>(.*?)<\/td>(<td>.*?<\/td>){8}/g;
 	result = re.exec(response);
-	while(result){
+	if(result){
 		id = result[1];	
+		console.log(id);
 		qiang(id);
 		result = re.exec(response);
 	}
